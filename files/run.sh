df9f671e902da250eedbd8911ce08b273ac861ea
Christian Berendt <berendt@betacloud-solutions.de>
2018-10-10 10:05:45 +0200

    Fix path to ara log file

--- files/run.sh
+++ files/run.sh
@@ -19,7 +19,7 @@ ARA_HOST=${ARA_HOST:-0.0.0.0}
 ARA_PORT=${ARA_PORT:-9191}
 export ARA_DATABASE=${ARA_DATABASE:-sqlite:////ara/ara.sqlite}
 export ARA_DIR=${ARA_DIR:-/ara}
-export ARA_LOG_FILE=${ARA_LOG_FILE:-/ara/ara.log}
+export ARA_LOG_FILE=${ARA_LOG_FILE:-ara.log}
 
 if [[ ! -z $ARA_CONFIG ]]; then
     export ARA_CONFIG
