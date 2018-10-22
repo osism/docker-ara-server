e12962285f2a5f8454b29d8f5e8c95ecd097be40
Christian Berendt <berendt@betacloud-solutions.de>
2018-10-22 18:35:57 +0200

    Drop license note from run.sh

--- files/run.sh
+++ files/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-# This file is subject to the terms and conditions defined in file 'LICENSE',
-# which is part of this repository.
-
 # Available environment variables
 #
 # ARA_CONFIG
